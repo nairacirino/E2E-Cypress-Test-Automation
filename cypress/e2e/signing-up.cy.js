b2f45f482052b93342ab7873b06f45ac80414a96
Naira Cirino <nacirino@gmail.com>
2022-11-10 23:24:16 -0300

    Create the automated test
    - Fixed the imports 
    - Created the constants to use the imported class methods
    - Wrote the tests for the remaining three pages.

--- cypress/e2e/signing-up.cy.js
+++ cypress/e2e/signing-up.cy.js
@@ -1,18 +1,17 @@
 /// <reference types="cypress" />
 
-import CreateNewAccount from '../support/pages/createNewAccount';
+import AddNewAddress from '../support/pages/addNewAdress/index';
+import AddressBook from '../support/pages/addressBook';
+import CreateNewAccount from '../support/pages/createNewAccount/index';
 import HomePage from '../support/pages/homePage/index'
-// import CreateNewAccount from '../support/pages/createNewAccount/index'
-// import AddNewAddress from '../support/pages/addNewAdress/index'
-// import AddressBook from '../support/pages/addressBook/index'
-// import MyAccount from '../support/pages/myAccount/index'
+import MyAccount from '../support/pages/myAccount/index'
 
 describe('Magento Software Testing Board - Demo Website ', () => {
     const homePage = new HomePage();
     const createNewAccount = new CreateNewAccount();
-    // const addNewAddress = new AddNewAddress();
-    // const addressBook = new AddressBook();
-    // const myAccount = new MyAccount();
+    const addNewAddress = new AddNewAddress();
+    const myAccount = new MyAccount();
+    const addressBook = new AddressBook();
 
     // context('Usuário Logado', () => {
         beforeEach(() => {
@@ -21,6 +20,9 @@ describe('Magento Software Testing Board - Demo Website ', () => {
         it('Criação de conta de usuário no ecommerce', () => {
             homePage.clickCreateAnAccountLink()
             createNewAccount.createAccount()
+            myAccount.openEditAddressPage()
+            addNewAddress.addDefaultAddress()
+            addressBook.goBackToMyAccount()
         })
     // })
 }); 
